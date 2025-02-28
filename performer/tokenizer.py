dc9e2e7df804acf0801ed89ed7e316e5327f4b95
anosharahim <anosharahim03@gmail.com>
2025-02-28 18:16:49 +0000

    add special tokens to vocab tokenizer

--- performer/tokenizer.py
+++ performer/tokenizer.py
@@ -4,10 +4,16 @@ from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
 def create_tokenizer(vocab_size):
+    special_tokens = ["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]", "[START]", "[END]"]
+    
     tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
     tokenizer.pre_tokenizer = Whitespace()
     trainer = BpeTrainer(
         vocab_size=vocab_size,
-        special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]", "[START]", "[END]"]
+        special_tokens=special_tokens
     )
+    
+    for token in special_tokens:
+        tokenizer.add_special_tokens([token])
+    
     return tokenizer, trainer
